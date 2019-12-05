c8127c27543538d59bcb397dceaea2fad5000278
Averi Johnson <averimj@live.com>
2019-12-04 21:06:30 -0700

    adds eventListener to the 'other' option

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ $(':input:enabled:visible:first').focus();
 
 // hides the textarea box
 $('#other-title').hide();
+
+
+/* adds an eventListener to the 'other' option using .change
+if user clicks on 'other', than a textarea box will appear */
+$('#title').change(function(){
+  const $value = $(this).val();
+  if ($value == 'other'){
+    $('#other-title').show();
+  } else {
+    $('#other-title').hide();
+  }
+});
